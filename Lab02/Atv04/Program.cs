6478c8b32ac39abc19734ac195e1cb0ca7122721
uJoao22 <joaopaniciobotelho@hotmail.com>
2022-03-16 20:25:14 -0300

    Quarta e quinta atividade

---
+++ Lab02/Atv04/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Atv04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Faça um programa que calcule e mostre a área de um trapézio. Sabe-se que: A = (( base maior - base menos) * altura) /2");
+
+            Console.WriteLine("Informe a base maior do trapézio: ");
+            double baseMaior = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe a base menor do trapézio: ");
+            double baseMenor = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe a altura do trapézio: ");
+            double altura = double.Parse(Console.ReadLine());
+
+            double area = ((baseMaior - baseMenor) * altura) / 2;
+
+            Console.WriteLine("A área do trapézio é: {0}", area);
+        }
+    }
+}
---
+++ Lab02/Atv05/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Atv05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Faça um programa que receba o valor do salário mínimo e o valor do salário de um funcionário, calcule e mostre a quantidade de salários mínimos que ganha esse funcionário.");
+
+            Console.WriteLine("Informe o valor do sálario minimo atualmente: ");
+            double salarioMinimo = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o valor do sálario de um funcionário: ");
+            double salarioFuncionario = double.Parse(Console.ReadLine()); 
+
+            double qtdSalario = salarioFuncionario/salarioMinimo;
+
+            Console.WriteLine("O funcionário ganha {0} salários mínimos",qtdSalario);
+        }
+    }
+}
