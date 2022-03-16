b671ef4e2b8bd505c88f232e722d12eaaff73239
João Pedro Anicio Botelho <1382123@sga.pucminas.br>
2022-03-16 19:32:06 -0300

    Primeira atividade

---
+++ Lab02/Atv01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Atv01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Faça um programa que receba dois números, calcule e mostre a subtração do primeiro número pelo segundo.");
+
+            Console.WriteLine("Insira o primeiro número: ");
+            double n1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o segundo número: ");
+            double n2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0} - {1} = {2}", n1, n2, n1-n2);
+        }
+    }
+}
