b882b2e73d0fd612e6be7468b4bb16d4dc919dfe
uJoao22 <joaopaniciobotelho@hotmail.com>
2022-03-24 16:15:13 -0300

    Lab03 - Atividade 7

---
+++ Lab03/Atv07/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Atv07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ler três números e informar a soma dos dois maiores. ");
+
+            Console.WriteLine("Informe o 1º número: ");
+            double n1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o 2º número: ");
+            double n2 = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Informe o 3º número: ");
+            double n3 = double.Parse(Console.ReadLine());
+
+            double maior=0, maior2=0;
+
+            if(n1>n2 && n1>n3)
+                maior = n1;
+            else if(n2>n1 && n2>n3)
+                maior = n2;
+            else
+                maior = n3;
+
+            if((n1>n2 && n1<n3) || (n1>n3 && n1<n2)) 
+                maior2 = n1;
+            else if((n2>n1 && n2<n3) || (n2>n3 && n2<n1))
+                maior2 = n2;
+            else 
+                maior2 = n3;
+            
+            Console.WriteLine(maior+" + "+maior2+" = "+ (maior+maior2));
+            
+        }
+    }
+}
