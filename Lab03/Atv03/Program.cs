bfd9a5bc0baf4a5823afa5eaac3765e53712f2f8
uJoao22 <joaopaniciobotelho@hotmail.com>
2022-03-24 15:45:39 -0300

    Lab03 - Atividade 3

---
+++ Lab03/Atv03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Atv03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ler um número inteiro e dizer se ele é par ou impar (dica: use o operador resto da divisão ) ");
+
+            Console.WriteLine("Informe um número inteiro: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if(num%2 == 0)
+                Console.WriteLine("PAR");
+            else
+                Console.WriteLine("IMPAR");
+        }
+    }
+}
