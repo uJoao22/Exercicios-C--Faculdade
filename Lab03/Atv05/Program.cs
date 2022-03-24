1023c2c62b9ff78664b488e5d9eab760a18e045d
uJoao22 <joaopaniciobotelho@hotmail.com>
2022-03-24 15:51:56 -0300

    Lab03 - Atividade 5

---
+++ Lab03/Atv05/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Atv05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ler um número e informar se ele é divisível por 2 e por 7 ao mesmo tempo. ");
+
+            Console.WriteLine("Informe um número inteiro: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num%14 == 0) 
+                Console.WriteLine("É divisível por 2 e 7");
+            else
+                Console.WriteLine("NÃO é divisível por 2 e 7");
+        }
+    }
+}
