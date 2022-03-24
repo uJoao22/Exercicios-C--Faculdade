774072a5727f813d1e0969a51110350096722f85
uJoao22 <joaopaniciobotelho@hotmail.com>
2022-03-24 17:47:58 -0300

    Lab03 - Atividade 4

---
+++ Lab03/Atv04/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Atv04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ler um número e informar seu valor absoluto ( Exemplo: ABS(10) = 10 / ABS(-10) = 10 ) ");
+            
+            Console.WriteLine("Informe um número inteiro: ");
+            int num = int.Parse(Console.ReadLine());
+
+            int valorAbs = Math.Abs(num);
+
+            Console.WriteLine("O valor absoluto é: {0}", valorAbs);
+        }
+    }
+}
