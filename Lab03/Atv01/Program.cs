201b549a0b88d6dba7c20ffdae36f4986a7b23c2
uJoao22 <joaopaniciobotelho@hotmail.com>
2022-03-24 15:38:33 -0300

    Lab02 - Atividade 1

---
+++ Lab03/Atv01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Atv01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ler dois números do teclado e informar o maior. ");
+
+            Console.WriteLine("Informe 1º número: ");
+            double n1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe 2º número: ");
+            double n2 = double.Parse(Console.ReadLine());
+
+            if(n1>n2) 
+                Console.WriteLine("{0} é maior que {1}", n1, n2);
+            else
+                Console.WriteLine("{0} é maior que {1}", n2, n1);
+        }
+    }
+}
