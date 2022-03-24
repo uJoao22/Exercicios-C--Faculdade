667a88e803aab715afdbd6d645994389733380d1
uJoao22 <joaopaniciobotelho@hotmail.com>
2022-03-24 15:42:19 -0300

    Lab03 - Atividade 2

---
+++ Lab03/Atv02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Atv02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ler um número real e informar se ele é positivo negativo ou nulo. ");
+
+            Console.WriteLine("Informe o número: ");
+            double num = double.Parse(Console.ReadLine());
+
+            if(num>0)
+                Console.WriteLine("POSITIVO");
+            else if(num<0)
+                Console.WriteLine("NEGATIVO");
+            else
+                Console.WriteLine("NULO");
+        }
+    }
+}
