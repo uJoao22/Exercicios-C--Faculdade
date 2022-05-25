5eb05b1c85aac13a8006f76eac9041af99756127
uJoao22 <joaopaniciobotelho@hotmail.com>
2022-05-25 19:42:59 -0300

    Lab09 - Atv01

--- Lab09/Atv01/Program.cs
+++ Lab09/Atv01/Program.cs
@@ -8,19 +8,27 @@ namespace Atv01
         {
             Console.WriteLine("Declare na função main() dois vetores de inteiros A e B. Usando a função acima, ler dois conjuntos de 10 números, colocando cada conj em um vetor. Depois prencha um terceiro vetor C[] sendo que cada posição de C[] deverá correponder ao menor valor correspondente às respectivas posições nos vetores A[] e B[]. Finalmente imprima o resultado de C[]. ");
 
-            int[] a = new int[10];
-            int[] b = new int[10];
+            float[] A = new float[10];
+            float[] B = new float[10];
 
-            int[] teste = LeVetor(a);
-            Console.WriteLine(teste);
+            LeVetor(A);
+            LeVetor(B);
+
+            ImprimeC(A, B);
         }
 
-        static int[] LeVetor(int []vetor) { 
-            for (int i=0; i<vetor.Length; i++) { 
-                Console.Write("Entre com a nota do aluno [{0}]", i+1);    
-                vetor[i] = int.Parse(Console.ReadLine()); 
-            } 
-            return vetor;
-        } 
+        static void LeVetor(float[] vetor) {
+            for (int i = 0; i < vetor.Length; i++) {
+                Console.Write("Entre com a nota do {0}° aluno: ", i+1);
+                vetor[i] = float.Parse(Console.ReadLine());
+            }
+        }
+
+        static void ImprimeC(float[] vetorA, float[] vetorB) {
+            for (int i=0; i<vetorA.Length; i++)
+                Console.WriteLine("A menor nota foi: {0}, do aluno: {1}",
+                vetorA[i] < vetorB[i] ? vetorA[i] : vetorB[i], vetorA[i] < vetorB[i] ? 'A' : 'B');
+        }
+
     }
 }
